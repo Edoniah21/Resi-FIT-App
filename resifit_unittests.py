d5fc4218c1b44f4d6af9f464d5e4e259c83cd8cd
jbddrexel <43096506+jbddrexel@users.noreply.github.com>
2022-11-13 18:08:53 -0500

    Add files via upload

--- resifit_unittests.py
+++ resifit_unittests.py
@@ -4,17 +4,35 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import unittest
 import requests
+from bs4 import BeautifulSoup
+
+# we will use selenium later to
+from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from shutil import copyfile
+from datetime import datetime, timedelta
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
 
 class TestResiFitWebsite(unittest.TestCase):
+    # setup for future Selenium use
+    # options = Options()
+    # options.headless = True
+    # driver = webdriver.Chrome(options=options)
 
     base = 'https://jbddrexel.wixsite.com/resifit'
 
     def test_home_page(self):
+        # self.driver.get('https://www.investopedia.com')
+
         response = requests.get(self.base)
-        # response.
-        # print(f'Sending get request to {self.base}...')
         self.assertEqual(response.status_code, 200)
-        # print(f'{self.base} returned a status code of 200. Success')
 
     # TODO rename this page from blank-1 to something else
     def test_about_page(self):
@@ -56,6 +74,22 @@ class TestResiFitWebsite(unittest.TestCase):
         response = requests.get(f'{self.base}/general-clean')
         self.assertEqual(response.status_code, 200)
 
+    def test_all_links_on_resources_page(self):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+        }
+        source = requests.get(f'{self.base}/blank-3').text
+        soup = BeautifulSoup(source, 'html.parser')
+        section = soup.find('section', {'id':'comp-la7mrnoo'})
+        links = section.find_all('a', href=True)
+        for link in links:
+            url = link.get('href')
+
+            # credit.com does not allow programmatic access, even when setting UserAgent in the headers to
+            # mimic browser activity. For now, we will skip testing the link for this site.
+            if url != 'https://www.credit.com':
+                response = requests.get(url, headers=headers)
+                self.assertEqual(response.status_code, 200)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
